1d660f57bf4e0a239908aca476f5ca0b7ee23399
Alexander Charles Winston-Clarke <gnert1111studio@gmail.com>
2014-05-01 15:09:32 +0300

    Initialize

---
+++ gQuery.js
@@ -0,0 +1,471 @@
+(function (global)
+{
+    "use strict";
+
+    var utilities = {
+        // High performance function check - http://jsperf.com/alternative-isfunction-implementations
+        isFunction: function (functionToCheck)
+        {
+            return !!(functionToCheck && functionToCheck.constructor && functionToCheck.call && functionToCheck.apply);
+        },
+
+        argumentsCallback: function (args, invoke)
+        {
+            var lastArgument = args[args.length - 1];
+
+            if (utilities.isFunction(lastArgument))
+            {
+                if (invoke)
+                {
+                    lastArgument();
+                }
+                else
+                {
+                    return lastArgument;
+                }
+            }
+            else
+            {
+                return undefined;
+            }
+        },
+        
+        mergeObjects: function( original, updates ) 
+        {
+            // Search for faster way.
+            for( var key in updates ) 
+            {
+                original[key] = updates[key];
+            }
+            
+            return original;
+        }
+    };
+    var gQuery = function (selector)
+    {
+        return new gQuery.func.initialize(selector);
+    };
+
+    gQuery.func = gQuery.prototype = {
+        constructor: gQuery,
+
+        /* 
+        @function initialize - Initializes gQuery elements with selector.
+        @param {String} selector - Selector to select element(s).
+        */
+        initialize: function (selector)
+        {
+            if ("querySelector" in document)
+            {
+                try
+                {
+                    if (!selector)
+                    {
+                        throw "ERROR: Invalid selector.";
+                    }
+                    else if (!!selector.nodeType && (selector.nodeType === 1 || selector.nodeType === 9))
+                    {
+                        this.els = [selector];
+                    }
+                    else if (typeof selector === "string")
+                    {
+                        this.els = document.querySelectorAll(selector);
+                    }
+                    else
+                    {
+                        throw "ERROR: Unable to query selector.";
+                    }
+
+                    this.length = this.els.length;
+
+                    if (this.length === 0)
+                    {
+                        throw "ERROR: No elements found with that selector.";
+                    }
+
+                    return this;
+                }
+                catch (e)
+                {
+                    throw e;
+                }
+            }
+            else
+            {
+                throw "ERROR: Not modern browser.";
+            }
+        },
+
+        /* 
+        @function each - Loops through elements and calls selected function within.
+        @param {Function} functionToCall - Function that will be called.
+        */
+        each: function (functionToCall)
+        {
+            var elements = this.els,
+                count = 0;
+
+            for (var i = 0, len = elements.length; i < len; i++)
+            {
+                (functionToCall.call(elements[i], i) === false) ? count-- : count++;
+            }
+
+            return count;
+        },
+
+        /*
+        @function hasClass - Determine whether any of the matched elements are assigned the given class.
+        @param {String} className - The class name to search for.
+        */
+        hasClass: function (className)
+        {
+            className = className.trim();
+
+            var result = this.each(function ()
+            {
+                return new RegExp(className)
+                    .test(this.className);
+            });
+
+            return ((result + this.length) > 0);
+        },
+
+        /*
+        @function addClass - Adds the specified class(es) to each of the set of matched elements.
+        @param {String} classNames - One or more space-separated classes to be added to the class attribute of each matched element.
+        */
+        addClass: function (classNames)
+        {
+            if (!classNames)
+            {
+                return;
+            }
+
+            var classesSplit = classNames.split(" ");
+
+            this.each(function ()
+            {
+                for (var i = 0; i < classesSplit.length; i++)
+                {
+                    if (this.classList)
+                    {
+                        this.classList.add(classesSplit[i]);
+                    }
+                    else
+                    {
+                        if (!this.hasClass(classesSplit[i]))
+                        {
+                            this.className += (" " + classesSplit[i]);
+                        }
+                    }
+                }
+            });
+
+            return this;
+        },
+
+        /*
+        @function removeClass - Remove a single class, multiple classes, or all classes from each element in the set of matched elements.
+        @param {String} classNames - One or more space-separated classes to be removed from the class attribute of each matched element.
+        */
+        removeClass: function (classNames)
+        {
+            if (!classNames)
+            {
+                return;
+            }
+
+            var classesSplit = classNames.split(" ");
+
+            this.each(function ()
+            {
+                for (var i = 0; i < classesSplit.length; i++)
+                {
+                    if (this.classList)
+                    {
+                        this.classList.remove(classesSplit[i]);
+                    }
+                    else
+                    {
+                        if (this.hasClass(classesSplit[i]))
+                        {
+                            var newClass = " " + this.className.replace(/[\t\r\n]/g, " ") + " ";
+
+                            while (newClass.indexOf(" " + classesSplit[i] + " ") >= 0)
+                            {
+                                newClass = newClass.replace(" " + classesSplit[i] + " ", " ");
+                            }
+
+                            this.className = nnewClass.replace(/^\s+|\s+$/g, " ");
+                        }
+                    }
+                }
+            });
+
+            return this;
+        },
+
+        /*
+        @function toggleClass - Add or remove one or more classes from each element in the set of matched elements, depending on either the class's presence or the value of the switch argument.
+        @param {String} classNames - One or more class names (separated by spaces) to be toggled for each element in the matched set.
+        */
+        toggleClass: function (className)
+        {
+            if (!className)
+            {
+                return;
+            }
+
+            this.each(function ()
+            {
+                if (this.classList)
+                {
+                    this.classList.toggle(className);
+                }
+                else
+                {
+                    if (this.hasClass(className))
+                    {
+                        this.removeClass(className);
+                    }
+                    else
+                    {
+                        this.addClass(className);
+                    }
+                }
+            });
+
+            return this;
+        },
+        /*
+        @function nth - Retrieves the DOM element(s).
+        @param {Number} index - A zero-based integer indicating which element to retrieve.
+        */
+        nth: function (index)
+        {
+            index = (index < 0 ? this.els.length + index : index);
+            return gQuery.func.initialize(this.els[index]);
+        },
+
+        /*
+        @function first - Retrieves the first DOM element.
+        */
+        first: function ()
+        {
+            return this.nth(0);
+        },
+
+        /*
+        @function last - Retrieves the last DOM element.
+        */
+        last: function ()
+        {
+            return this.nth(-1);
+        },
+
+        /*
+        @function remove -  Removes selected elements from the DOM.
+        */
+        remove: function ()
+        {
+            this.each(function ()
+            {
+                if (this.parentElement)
+                {
+                    this.parentElement.removeChild(this);
+                }
+            });
+
+            return this;
+        },
+
+        /*
+        @function show - Displays selected elements in the DOM.
+        */
+        show: function ()
+        {
+            this.each(function ()
+            {
+                this.style.display = "";
+            });
+
+            return this;
+        },
+
+        /*
+        @function hide - Hides selected elements in the DOM.
+        */
+        hide: function ()
+        {
+            this.each(function ()
+            {
+                this.style.display = "none";
+            });
+
+            return this;
+        },
+
+        /*
+        @function attr - Get the value of an attribute for the first element in the set of matched elements or set one or more attributes for every matched element.
+        @param {String} attributeName - The name of the attribute to get or set.
+        @param {String} value - A value to set for the attribute.
+        */
+        attr: function (attributeName, value)
+        {
+            if (!value)
+            {
+                return this.els[0].getAttribute(attributeName);
+            }
+            else
+            {
+                this.each(function ()
+                {
+                    this.setAttribute(attributeName, value);
+                });
+            }
+
+            return this;
+        },
+
+        /*
+        @function html - Get the HTML contents of the first element in the set of matched elements or set the HTML contents of every matched element.
+        @param {String} html - A string of HTML to set as the content of each matched element.
+        */
+        html: function (html)
+        {
+            if (typeof html === "string")
+            {
+                this.each(function ()
+                {
+                    this.innerHTML = html;
+                });
+
+                return this;
+            }
+            else
+            {
+                return this.els[0].innerHTML;
+            }
+        },
+
+        /*
+        @function html - Get the combined text contents of each element in the set of matched elements, including their descendants, or set the text contents of the matched elements.
+        @param {String} html - The text to set as the content of each matched element.
+        */
+        text: function (text)
+        {
+            if (typeof text === "string")
+            {
+                this.each(function ()
+                {
+                    if (typeof this.textContent !== "undefined")
+                    {
+                        this.textContent = text;
+                    }
+                    else
+                    {
+                        this.innerText = text;
+                    }
+                });
+
+                return this;
+            }
+            else
+            {
+                if (typeof this.els[0].textContent !== "undefined")
+                {
+                    return this.els[0].textContent;
+                }
+                else
+                {
+                    return this.els[0].innerText;
+                }
+            }
+        },
+
+        /*
+        @function on - Attach an event handler function for one or more events to the selected elements.
+        @param {String} events - One or more space-separated event types and optional namespaces, or just namespaces, such as "click".
+        @arguments {Function} - A function to execute when the event is triggered.
+        */
+        on: function (events)
+        {
+            var eventsSplit = events.split(" "),
+                eventFunction = utilities.argumentsCallback(arguments);
+
+            this.each(function ()
+            {
+                for (var i = 0; i < eventsSplit.length; i++)
+                {
+                    if (this.addEventListener)
+                    {
+                        this.addEventListener(eventsSplit[i], eventFunction, false);
+                    }
+                    else if (this.attachEvent)
+                    {
+                        if (this.window && (eventsSplit[i].indexOf("mouse") != -1 || eventsSplit[i].indexOf("click") != -1))
+                        {
+                            document.attachEvent("on" + eventsSplit[i], eventFunction);
+                        }
+                        else
+                        {
+                            this.attachEvent("on" + eventsSplit[i], eventFunction);
+                        }
+                    }
+                }
+            });
+
+            return this;
+        },
+
+        /*
+        @function on - Remove an event handler.
+        @param {String} events - One or more space-separated event types and optional namespaces, or just namespaces, such as "click".
+        @arguments {Function} - A handler function previously attached for the event(s).
+        */
+        off: function (events)
+        {
+            var eventsSplit = events.split(" "),
+                eventFunction = utilities.argumentsCallback(arguments);
+
+            this.each(function ()
+            {
+                for (var i = 0; i < eventsSplit.length; i++)
+                {
+                    if (this.removeEventListener)
+                    {
+                        this.removeEventListener(eventsSplit[i], eventFunction, false);
+                    }
+                    else if (this.detachEvent)
+                    {
+                        if (this.window && (eventsSplit[i].indexOf("mouse") != -1 || eventsSplit[i].indexOf("click") != -1))
+                        {
+                            document.detachEvent("on" + eventsSplit[i], eventFunction);;
+                        }
+                        else
+                        {
+                            this.detachEvent("on" + eventsSplit[i], eventFunction);
+                        }
+                    }
+                }
+            });
+
+            return this;
+        },
+
+        /*
+        @function click - Bind an event handler to the "click" JavaScript event, or trigger that event on an element.
+        @param {Function} callback - A function to execute when the event is triggered.
+        */
+        click: function (callback)
+        {
+            this.on("click", callback);
+        }
+    };
+
+    gQuery.func.initialize.prototype = gQuery.func;
+
+    global.gQuery = gQuery;
+
+    global.$ = gQuery;
+}(this));
